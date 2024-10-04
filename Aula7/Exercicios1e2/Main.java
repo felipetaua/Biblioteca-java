ba1234d78bd2a8225735c445b350b9940407f040
Tauã Felipe <127506090+felipetaua@users.noreply.github.com>
2024-10-04 08:22:45 -0300

    Add files via upload

---
+++ Aula7/Exercicios1e2/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // Exercício 1 - descobrir o numero maior
+
+    // Slicitando o primeiro numero
+    // System.out.println("Digite um numero: ");
+    // double numero = scanner.nextDouble();
+    // if(numero >= 0) {
+    // System.out.println("O numero é positivo");
+    // } else {
+    // System.out.println("O numero é negativo");
+    // }
+    // scanner.close();
+
+    // Exercício 2 - comparador de numeros
+
+    // Solicitando o primeiro numero
+    // System.out.println("Digite o primeiro numero");
+    // int numero1 = scanner.nextInt();
+
+    // System.out.println("Digite o segundo numero");
+    // int numero2 = scanner.nextInt();
+
+    // // Comparando os dois numeros
+    // if (numero1 == numero2) {
+    //   System.out.println("Os numeros são iguais");
+    // } else if (numero1 > numero2) {
+    //   System.out.println("O numero 1 é maior que o numero 2");
+    // } else {
+    //   System.out.println("O numero 2 é maior que o numero 1");
+    // }
+    // scanner.close();
+
+    // Exercício 3 - area do quadrado ou area do circulo
+
+    
+  }
+}
